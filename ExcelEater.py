c7ed2c13ca31e3d57e64d5f50720e548614630a8
ChimeraBlack1 <christopherparke@zoho.com>
2020-02-27 16:08:13 -0500

    the program now takes a master or child sheet and extracts the values from the index column. it also takes a dynamic number of arguments to target other columns to pair with the index column

--- ExcelEater.py
+++ ExcelEater.py
@@ -54,20 +54,30 @@ def FindLastRow(sheet, row=0, col=0):
   return rowCount
 
 
-def GetValues(sheet, col, row, end):
+def GetValues(sheet, whichCol, index, start, end):
   """
   Return a nested dictionary of values from the target sheet
+  create object of objects that contain the value to update as the key, and the values and xl cell as the details
+  ie: {123456: {xlRow:144, xlCol:5,color: "blue", type:"sale", angle:90}, 890128:{xlRow:145, xlCol:5, color: "red", type:"return", angle:180}}
   """
-  # create object of objects that contain the value to update as the key, and the values and xl cell as the details
-  # ie: {123456: {xlRow:144, xlCol:5,color: "blue", type:"sale", angle:90}, 890128:{xlRow:145, xlCol:5, color: "red", type:"return", angle:180}}
+
   destDict = {}
 
-  for i in range(row, end):
-    val = sheet.cell(i,col).value
-    destDict[i] = val
-  
+  for i in range(start, end):
+    indexVal = sheet.cell(i,index).value
+    for j in whichCol:
+      print(j)
+
+    destDict[indexVal] = {
+      
+    }
+    #TODO change val to be the key rather than i
+
   return destDict
 
+
+def ConsumeChild():
+  pass
 
 def GetInput(message, err):
   """
@@ -115,7 +125,7 @@ def GetSheet(book, prompt):
   return sheet
 
 
-def ValidateMaxRow():
+def ValidateMaxRow(sheet):
   """
   Did the user enter a valid ENDING cell?
   """
@@ -123,8 +133,8 @@ def ValidateMaxRow():
   while valid == False:
     maxRow_ = GetInput("Please enter the row you want to read the excel sheet to ", "Sorry, that is not a valid row. Please try again... ")
     if maxRow_ == "":
-      print("Fine, don't tell me...I'll just guess...")
-      maxRow_ = masterSheet.max_row +1
+      print("Searching entire document...")
+      maxRow_ = sheet.max_row +1
       valid = True
     try:
       maxRow_ = int(maxRow_)
@@ -133,86 +143,91 @@ def ValidateMaxRow():
       print("you must enter a valid number")
       continue
     if maxRow_ < 1:
-      print("You did not specify a valid number. The program will guess at the which row is the END")
-      maxRow_ = masterSheet.max_row +1
+      print("Searching entire document...")
+      maxRow_ = sheet.max_row +1
       valid = True
   return maxRow_
 
 
 
-def ValidateMasterRowStart():
+def ValidateRowStart():
   """
   Did the user enter a valid STARTING cell?
   """
   valid = False
   while valid == False:
-    masterRowStart_ = GetInput("Which row would you like to start on?", "Sorry that's not a valid row.  Please try again ")
-    if masterRowStart_ == "":
-      print("If you're not gonna tell me, I'm just gonna search the whole thing...")
-      masterRowStart_ = 1
+    rowStart_ = GetInput("Which row would you like to start on?", "Sorry that's not a valid row.  Please try again ")
+    if rowStart_ == "":
+      print("Starting at row 1...")
+      rowStart_ = 1
       valid = True
     try:
-      int(masterRowStart_)
+      int(rowStart_)
       valid = True
     except:
       print("you must enter a valid number")
       continue
-    if masterRowStart_ < 1:
-      print("You did not specify a valid number. The program will guess at the which row is the START")
-      masterRowStart_ = 1
+    if rowStart_ <= 0:
+      print("Starting at row 1...")
+      rowStart_ = 0
       valid = True
-  return masterRowStart_
+  return rowStart_
 
 
-def ValidateMasterCol():
-    """
-  Did the user enter a VALID INDEX COLUMN for the MASTER?
+def ValidateCol():
+  """
+  Did the user enter a VALID INDEX COLUMN?
   """
   valid = False
   while valid == False:
-    masterRowStart_ = GetInput("Which column are we going to use for the master index?", "Sorry that's not a valid row.  Please try again ")
-    if masterRowStart_ == "":
-      print("If you're not gonna tell me, I'm just gonna search the whole thing...")
-      masterRowStart_ = 1
+    colStart_ = GetInput("Which column are we going to use for the index?", "Sorry that's not a valid row.  Please try again ")
+    if colStart_ == "":
+      print("Searching entire document...")
+      colStart_ = 1
       valid = True
     try:
-      int(masterRowStart_)
+      colStart_ = int(colStart_)
       valid = True
     except:
       print("you must enter a valid number")
       continue
-    if masterRowStart_ < 1:
+    if colStart_ < 1:
       print("You did not specify a valid number. The program will guess at the which row is the START")
-      masterRowStart_ = 1
+      colStart_ = 1
       valid = True
-  return masterRowStart_
+  return colStart_
 
 
+def GetColumns(colCount):
+  """
+  Return an object containing the names and locations of the columns to retrieve data from
+  """
+  goodData = False
+  whichCol = {}
+  while goodData == False:
+    for i in range(0, colCount):
+      colName = GetInput("Name column " +str(i+1), "Invalid column name")
+      colNum = GetInput("Enter Column number for value " +str(i+1), "Invalid column number")
+      try:
+        colNum = int(colNum)
+        goodData = True
+      except:
+        print("Invalid column number...")
+      whichCol[colNum] = colName
+    
+  return whichCol
 
-"""
-                Home
--------------------------------------
-1) Add a Master sheet
-      - what is the name of the book
-      - what is the name of the sheet
-      - column(s) to match against child (index column)
-2) Add Child Sheet
-      - what is the name of the book
-      - what is the name of the sheet
-      - column(s) to match against master (index column)
-      - column(s) (values) to copy/paste into master
-3) Consume Child Sheet
-      - overwrite master entries?
-      - save updated master sheet
-4) Print Master values
-5) Print Child values
-6) Print values in Child not in Master
-7) Print values in Master not in Child
-8) Print values consumed by Master (and from which child)
+def UploadSuccessful(sheet):
+  if sheet == "master".lower():
+    print("***************************************")
+    print("*Master document uploaded successfully*")
+    print("***************************************")
+  if sheet == "child".lower():
+    print("**************************************")
+    print("*Child document uploaded successfully*")
+    print("**************************************")
+  
 
-*** TYPE EXIT TO STOP THE PROGRAM *** 
-
-"""
 #master globals
 masterBook = {}
 masterSheet = {}
@@ -241,15 +256,26 @@ while run:
     #get master sheet
     masterBook = GetBook("What is the name of the MASTER book? ")
     masterSheet = GetSheet(masterBook, "What is the name of the target sheet in the MASTER book? ")
-    maxRow_ = ValidateMaxRow()
-    masterRowStart_ = ValidateMasterRowStart()
-    masterCol = ValidateMasterCol()
+    maxRow_ = ValidateMaxRow(masterSheet)
+    masterRowStart_ = ValidateRowStart()
+    masterColIndex = ValidateCol()
+    colCount = GetInput("how many columns are we taking values from?","Sorry that's not a valid number.  Try again.")
+    # colCount = int(colCount)
+    whichCol = GetColumns(int(colCount))
+    print(whichCol)
 
-    masterValues = GetValues(masterSheet, masterCol, masterRowStart_, maxRow_)
+    masterValues = GetValues(masterSheet, whichCol, masterColIndex, masterRowStart_, maxRow_)
+    UploadSuccessful(sheet="master")
   elif selection == "2":
     #get child sheet
-    childBook = GetBook("What is the name of the CHILD book? ")
-    childSheet = GetSheet(childBook, "What is the name of the target sheet in the CHILD book? ")
+    childBook_ = GetBook("What is the name of the CHILD book? ")
+    childSheet_ = GetSheet(childBook_, "What is the name of the target sheet in the CHILD book? ")
+    maxRow_ = ValidateMaxRow(childSheet_)
+    childRowStart_ = ValidateRowStart()
+    childCol_ = ValidateCol()
+
+    childValues = GetValues(childSheet_, childCol_, childRowStart_, maxRow_)
+    UploadSuccessful(sheet="child")
   elif selection == "3":
     #consume child sheet
       # column(s) to match against master (index column)
@@ -257,9 +283,16 @@ while run:
     pass
   elif selection == "4":
     # print master values
-    print(masterValues)
+    try:
+      print(masterValues)
+    except:
+      print("no Master sheet has been uploaded yet")
   elif selection == "5":
     # print child values
+    try:
+      print(childValues)
+    except:
+      print("no Child sheet has been uploaded yet...")
       #select child
     pass
 
